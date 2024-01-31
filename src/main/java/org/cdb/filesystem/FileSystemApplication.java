f6881483d0cb7f74a110b17c0697d9c225c09f1e
RodrigoFAbreu <rodrigo-f-a@hotmail.com>
2024-01-31 15:31:39 +0000

    first commit

---
+++ src/main/java/org/cdb/filesystem/FileSystemApplication.java
@@ -0,0 +1,13 @@
+package org.cdb.filesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSystemApplication.class, args);
+    }
+
+}
